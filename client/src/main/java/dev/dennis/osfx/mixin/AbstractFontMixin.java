e853b82a97a35f23262bf06546055953b4539a36
Dennis <me@dennis.dev>
2019-12-25 04:10:12 +0100

    add font glyphs getter

--- client/src/main/java/dev/dennis/osfx/mixin/AbstractFontMixin.java
+++ client/src/main/java/dev/dennis/osfx/mixin/AbstractFontMixin.java
@@ -1,8 +1,12 @@
 package dev.dennis.osfx.mixin;
 
 import dev.dennis.osfx.api.AbstractFont;
+import dev.dennis.osfx.inject.mixin.Getter;
 import dev.dennis.osfx.inject.mixin.Mixin;
 
 @Mixin("AbstractFont")
 public abstract class AbstractFontMixin implements AbstractFont {
+    @Getter("glyphs")
+    @Override
+    public abstract byte[][] getGlyphs();
 }
