6dfccfc537b50dfef0d1cf32f76aa1954f9c7a7b
Dennis <me@dennis.dev>
2019-12-25 03:39:05 +0100

    add abstract font mixin

---
+++ client/src/main/java/dev/dennis/osfx/api/AbstractFont.java
@@ -0,0 +1,4 @@
+package dev.dennis.osfx.api;
+
+public interface AbstractFont {
+}
---
+++ client/src/main/java/dev/dennis/osfx/mixin/AbstractFontMixin.java
@@ -0,0 +1,8 @@
+package dev.dennis.osfx.mixin;
+
+import dev.dennis.osfx.api.AbstractFont;
+import dev.dennis.osfx.inject.mixin.Mixin;
+
+@Mixin("AbstractFont")
+public abstract class AbstractFontMixin implements AbstractFont {
+}
