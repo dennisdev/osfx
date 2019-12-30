03b478f7eecc78c6217ad375e31dc05921c39630
Dennis <me@dennis.dev>
2019-12-30 12:31:45 +0100

    add widget mixin

---
+++ client/src/main/java/dev/dennis/osfx/mixin/WidgetMixin.java
@@ -0,0 +1,8 @@
+package dev.dennis.osfx.mixin;
+
+import dev.dennis.osfx.api.Widget;
+import dev.dennis.osfx.inject.mixin.Mixin;
+
+@Mixin("Widget")
+public abstract class WidgetMixin implements Widget {
+}
