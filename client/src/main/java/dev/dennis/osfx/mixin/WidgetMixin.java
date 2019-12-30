8a83b9562300c2b4589b311af3b5592dd5ef35fb
Dennis <me@dennis.dev>
2019-12-30 12:39:55 +0100

    add widget width getter

--- client/src/main/java/dev/dennis/osfx/mixin/WidgetMixin.java
+++ client/src/main/java/dev/dennis/osfx/mixin/WidgetMixin.java
@@ -1,8 +1,12 @@
 package dev.dennis.osfx.mixin;
 
 import dev.dennis.osfx.api.Widget;
+import dev.dennis.osfx.inject.mixin.Getter;
 import dev.dennis.osfx.inject.mixin.Mixin;
 
 @Mixin("Widget")
 public abstract class WidgetMixin implements Widget {
+    @Getter("width")
+    @Override
+    public abstract int getWidth();
 }
