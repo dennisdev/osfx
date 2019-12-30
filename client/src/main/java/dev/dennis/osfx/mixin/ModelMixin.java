7302b334a88b2141ac329c46e2a6402dbc690413
Dennis <me@dennis.dev>
2019-12-30 16:50:42 +0100

    add model verticesX getter

--- client/src/main/java/dev/dennis/osfx/mixin/ModelMixin.java
+++ client/src/main/java/dev/dennis/osfx/mixin/ModelMixin.java
@@ -1,8 +1,12 @@
 package dev.dennis.osfx.mixin;
 
+import dev.dennis.osfx.inject.mixin.Getter;
 import dev.dennis.osfx.inject.mixin.Mixin;
 import dev.dennis.osfx.api.Model;
 
 @Mixin("Model")
 public abstract class ModelMixin implements Model {
+    @Getter("verticesX")
+    @Override
+    public abstract int[] getVerticesX();
 }
