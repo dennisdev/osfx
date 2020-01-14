b015fc393ac1a5c0128686a70220cb39b93425e3
Dennis <me@dennis.dev>
2020-01-14 22:07:45 +0100

    add lwjgl and bgfx debug options

--- client/src/main/java/dev/dennis/osfx/Main.java
+++ client/src/main/java/dev/dennis/osfx/Main.java
@@ -5,6 +5,7 @@ import dev.dennis.osfx.util.OsrsConfig;
 
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -13,7 +14,20 @@ public class Main {
 
     private static final boolean LOAD_NEW_CONFIG = false;
 
+    private static final boolean LWJGL_DEBUG = true;
+
+    private static final boolean BGFX_DEBUG = true;
+
+    private static final String BGFX_DEBUG_DLL_NAME = "bgfx_debug.dll";
+
     public static void main(String[] args) throws Exception {
+        if (LWJGL_DEBUG) {
+            System.setProperty("org.lwjgl.util.Debug", "true");
+        }
+        if (BGFX_DEBUG && Files.exists(Paths.get(BGFX_DEBUG_DLL_NAME))) {
+            System.setProperty("org.lwjgl.bgfx.libname", "bgfx_debug.dll");
+        }
+
         OsrsConfig config;
         if (LOAD_NEW_CONFIG) {
             config = OsrsConfig.load("http://oldschool83.runescape.com/jav_config.ws");
