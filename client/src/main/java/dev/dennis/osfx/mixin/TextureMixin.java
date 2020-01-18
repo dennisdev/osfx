78873f96d270aaa07bf06da3fb787c6cc11a5a1c
Dennis <me@dennis.dev>
2020-01-18 09:26:55 +0100

    Add texture mixin

---
+++ client/src/main/java/dev/dennis/osfx/mixin/TextureMixin.java
@@ -0,0 +1,8 @@
+package dev.dennis.osfx.mixin;
+
+import dev.dennis.osfx.api.Texture;
+import dev.dennis.osfx.inject.mixin.Mixin;
+
+@Mixin("Texture")
+public abstract class TextureMixin implements Texture {
+}
