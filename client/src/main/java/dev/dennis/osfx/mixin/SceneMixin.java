b945c140fccd36a672bc450e9b07e9e035d69c67
Dennis <me@dennis.dev>
2020-01-18 06:12:17 +0100

    Add scene mixin

---
+++ client/src/main/java/dev/dennis/osfx/mixin/SceneMixin.java
@@ -0,0 +1,8 @@
+package dev.dennis.osfx.mixin;
+
+import dev.dennis.osfx.api.Scene;
+import dev.dennis.osfx.inject.mixin.Mixin;
+
+@Mixin("Scene")
+public abstract class SceneMixin implements Scene {
+}
