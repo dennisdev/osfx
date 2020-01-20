55c94425a4b6806ce0b1a26ddf2336c9c530426e
Dennis <me@dennis.dev>
2020-01-20 06:14:38 +0100

    Add texture provider mixin

---
+++ client/src/main/java/dev/dennis/osfx/mixin/TextureProviderMixin.java
@@ -0,0 +1,8 @@
+package dev.dennis.osfx.mixin;
+
+import dev.dennis.osfx.api.TextureProvider;
+import dev.dennis.osfx.inject.mixin.Mixin;
+
+@Mixin("TextureProvider")
+public abstract class TextureProviderMixin implements TextureProvider {
+}
