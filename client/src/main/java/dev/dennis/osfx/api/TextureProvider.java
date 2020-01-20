8123e3d5344b86bc9d99fc96005be5d09cffc6cc
Dennis <me@dennis.dev>
2020-01-20 06:59:25 +0100

    Add texture provider load method

--- client/src/main/java/dev/dennis/osfx/api/TextureProvider.java
+++ client/src/main/java/dev/dennis/osfx/api/TextureProvider.java
@@ -1,5 +1,7 @@
 package dev.dennis.osfx.api;
 
 public interface TextureProvider {
+    int[] load(int id);
+
     Texture[] getTextures();
 }
--- client/src/main/java/dev/dennis/osfx/mixin/TextureProviderMixin.java
+++ client/src/main/java/dev/dennis/osfx/mixin/TextureProviderMixin.java
@@ -3,10 +3,15 @@ package dev.dennis.osfx.mixin;
 import dev.dennis.osfx.api.Texture;
 import dev.dennis.osfx.api.TextureProvider;
 import dev.dennis.osfx.inject.mixin.Getter;
+import dev.dennis.osfx.inject.mixin.Invoke;
 import dev.dennis.osfx.inject.mixin.Mixin;
 
 @Mixin("TextureProvider")
 public abstract class TextureProviderMixin implements TextureProvider {
+    @Invoke("load")
+    @Override
+    public abstract int[] load(int id);
+
     @Getter("textures")
     @Override
     public abstract Texture[] getTextures();
