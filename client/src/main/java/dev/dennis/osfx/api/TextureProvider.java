d84ce2fae7cb340e1975c4b1368bb3188131b757
Dennis <me@dennis.dev>
2020-01-20 06:35:13 +0100

    Add texture provider textures getter

--- client/src/main/java/dev/dennis/osfx/api/TextureProvider.java
+++ client/src/main/java/dev/dennis/osfx/api/TextureProvider.java
@@ -1,4 +1,5 @@
 package dev.dennis.osfx.api;
 
 public interface TextureProvider {
+    Texture[] getTextures();
 }
--- client/src/main/java/dev/dennis/osfx/mixin/TextureProviderMixin.java
+++ client/src/main/java/dev/dennis/osfx/mixin/TextureProviderMixin.java
@@ -1,8 +1,13 @@
 package dev.dennis.osfx.mixin;
 
+import dev.dennis.osfx.api.Texture;
 import dev.dennis.osfx.api.TextureProvider;
+import dev.dennis.osfx.inject.mixin.Getter;
 import dev.dennis.osfx.inject.mixin.Mixin;
 
 @Mixin("TextureProvider")
 public abstract class TextureProviderMixin implements TextureProvider {
+    @Getter("textures")
+    @Override
+    public abstract Texture[] getTextures();
 }
