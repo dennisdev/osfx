7aac1a6bd36c596d92f8183182e522752ab3b65d
Dennis <me@dennis.dev>
2020-01-21 11:25:55 +0100

    Add scene tile paint getters

--- client/src/main/java/dev/dennis/osfx/api/SceneTilePaint.java
+++ client/src/main/java/dev/dennis/osfx/api/SceneTilePaint.java
@@ -1,4 +1,15 @@
 package dev.dennis.osfx.api;
 
 public interface SceneTilePaint {
+    int getSwColor();
+
+    int getSeColor();
+
+    int getNeColor();
+
+    int getNwColor();
+
+    int getTextureId();
+
+    int getRgb();
 }
--- client/src/main/java/dev/dennis/osfx/mixin/SceneTilePaintMixin.java
+++ client/src/main/java/dev/dennis/osfx/mixin/SceneTilePaintMixin.java
@@ -1,8 +1,32 @@
 package dev.dennis.osfx.mixin;
 
 import dev.dennis.osfx.api.SceneTilePaint;
+import dev.dennis.osfx.inject.mixin.Getter;
 import dev.dennis.osfx.inject.mixin.Mixin;
 
 @Mixin("SceneTilePaint")
 public abstract class SceneTilePaintMixin implements SceneTilePaint {
+    @Getter("swColor")
+    @Override
+    public abstract int getSwColor();
+
+    @Getter("seColor")
+    @Override
+    public abstract int getSeColor();
+
+    @Getter("neColor")
+    @Override
+    public abstract int getNeColor();
+
+    @Getter("nwColor")
+    @Override
+    public abstract int getNwColor();
+
+    @Getter("textureId")
+    @Override
+    public abstract int getTextureId();
+
+    @Getter("rgb")
+    @Override
+    public abstract int getRgb();
 }
