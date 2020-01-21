599d7942ada44adc4299efd9be6902206bc1cf9a
Dennis <me@dennis.dev>
2020-01-21 11:06:59 +0100

    Add scene tile paint mixin

---
+++ client/src/main/java/dev/dennis/osfx/mixin/SceneTilePaintMixin.java
@@ -0,0 +1,8 @@
+package dev.dennis.osfx.mixin;
+
+import dev.dennis.osfx.api.SceneTilePaint;
+import dev.dennis.osfx.inject.mixin.Mixin;
+
+@Mixin("SceneTilePaint")
+public abstract class SceneTilePaintMixin implements SceneTilePaint {
+}
