ef1610974dde6f09d2c99f3f33eb3d1643ae3584
Dennis <me@dennis.dev>
2020-01-22 05:52:41 +0100

    Add scene tile model getters

--- client/src/main/java/dev/dennis/osfx/api/SceneTileModel.java
+++ client/src/main/java/dev/dennis/osfx/api/SceneTileModel.java
@@ -1,4 +1,23 @@
 package dev.dennis.osfx.api;
 
 public interface SceneTileModel {
+    int[] getVerticesX();
+
+    int[] getVerticesY();
+
+    int[] getVerticesZ();
+
+    int[] getColorsA();
+
+    int[] getColorsB();
+
+    int[] getColorsC();
+
+    int[] getIndicesA();
+
+    int[] getIndicesB();
+
+    int[] getIndicesC();
+
+    int[] getTextureIds();
 }
--- client/src/main/java/dev/dennis/osfx/mixin/SceneTileModelMixin.java
+++ client/src/main/java/dev/dennis/osfx/mixin/SceneTileModelMixin.java
@@ -1,8 +1,48 @@
 package dev.dennis.osfx.mixin;
 
 import dev.dennis.osfx.api.SceneTileModel;
+import dev.dennis.osfx.inject.mixin.Getter;
 import dev.dennis.osfx.inject.mixin.Mixin;
 
 @Mixin("SceneTileModel")
 public abstract class SceneTileModelMixin implements SceneTileModel {
+    @Getter("verticesX")
+    @Override
+    public abstract int[] getVerticesX();
+
+    @Getter("verticesY")
+    @Override
+    public abstract int[] getVerticesY();
+
+    @Getter("verticesZ")
+    @Override
+    public abstract int[] getVerticesZ();
+
+    @Getter("colorsA")
+    @Override
+    public abstract int[] getColorsA();
+
+    @Getter("colorsB")
+    @Override
+    public abstract int[] getColorsB();
+
+    @Getter("colorsC")
+    @Override
+    public abstract int[] getColorsC();
+
+    @Getter("indicesA")
+    @Override
+    public abstract int[] getIndicesA();
+
+    @Getter("indicesB")
+    @Override
+    public abstract int[] getIndicesB();
+
+    @Getter("indicesC")
+    @Override
+    public abstract int[] getIndicesC();
+
+    @Getter("textureIds")
+    @Override
+    public abstract int[] getTextureIds();
 }
