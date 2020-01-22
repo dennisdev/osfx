a040256015f4b8ad8cccf20bff34796c354d6e96
Dennis <me@dennis.dev>
2020-01-22 05:28:37 +0100

    Add scene tile model mixin

---
+++ client/src/main/java/dev/dennis/osfx/mixin/SceneTileModelMixin.java
@@ -0,0 +1,8 @@
+package dev.dennis.osfx.mixin;
+
+import dev.dennis.osfx.api.SceneTileModel;
+import dev.dennis.osfx.inject.mixin.Mixin;
+
+@Mixin("SceneTileModel")
+public abstract class SceneTileModelMixin implements SceneTileModel {
+}
