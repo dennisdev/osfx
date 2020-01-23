a5e23bbee2eaea8a20e762107ec7d35989b99847
Dennis <me@dennis.dev>
2020-01-23 08:07:33 +0100

    Add tile mixin

---
+++ client/src/main/java/dev/dennis/osfx/mixin/TileMixin.java
@@ -0,0 +1,8 @@
+package dev.dennis.osfx.mixin;
+
+import dev.dennis.osfx.api.Tile;
+import dev.dennis.osfx.inject.mixin.Mixin;
+
+@Mixin("Tile")
+public abstract class TileMixin implements Tile {
+}
